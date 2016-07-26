db34c6ec0dedba1bf248c677f6159f97e4cf9c8c
ammarhasan94 <hasanammar94@gmail.com>
2016-07-26 15:16:02 +0500

    Check Authorization on every query, Implement refresh token, Change public interface hence Example project

--- example/index.js
+++ example/index.js
@@ -2,44 +2,46 @@ var http = require('http'),
     port = process.env.PORT || 4000,
     express = require('express'),
     app = express(),
+    session = require('express-session'),
     Sage = require('sageone-wrapper');
 
 
 var config = {
-  "sageClientId": "***yourClientId***",
-  "sageClientSecret": "***yourClientSecret",
-  "sageSigningSecret": "***yourSigningSecret***",
-  "sageRedirectUri": "http://localhost:"+port+"/callback/" //Add a Redirect Uri in your SageOne app
+    "sageClientId": "***yourClientId***",
+    "sageClientSecret": "***yourClientSecret",
+    "sageSigningSecret": "***yourSigningSecret***",
+    "sageRedirectUri": "http://localhost:" + port + "/callback/" //Add a Redirect Uri in your SageOne app
 }
 
 var sageRef = new Sage(config.sageClientId, config.sageClientSecret, config.sageSigningSecret, config.sageRedirectUri);
 
+app.use(session({ resave: false, saveUninitialized: false, secret: 'ammar' }));
+
 // GET: /
 app.get("/", function (req, res) {
+    if (req.session.sageAuthCode)
+        res.redirect('../ledgerAccounts')
     res.redirect(sageRef.getAuthUrl());
 });
 
 // GET: callback/
 app.get("/callback", function (req, res) {
-    sageRef.authorize(req.query.code, function (err, data) {
-        if (err) {
-            console.log('Error:', err);
-        } else {
-            res.redirect('../ledgerAccounts');
-        }
-    });
+    sageRef.setAuthCode(req.query.code);
+    req.session.sageAuthCode = req.query.code;
+    res.redirect('../ledgerAccounts');
 });
 
 // GET: ledgerAccounts/
 app.get("/ledgerAccounts", function (req, res) {
+    if (!req.session.sageAuthCode)
+        res.redirect('../');
     sageRef.query('GET', 'ledger_accounts', {}, function (err, data) {
         console.log(err)
         if (err) {
             console.log(err);
-            return res.json(400, err);
+            return res.status(400).json(err);
         } else {
             console.log("Success");
-            data = JSON.parse(data);
             console.log("Data:", data);
             return res.send(data["$resources"]);
         }
